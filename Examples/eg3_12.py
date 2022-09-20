89a4ad79029eddceeae658d644378f8e8009d605
Sai Thant Min Nyo <saithantminnyo2@gmail.com>
2022-09-20 23:49:48 +0700

    eg3_12 finish

--- Examples/eg3_12.py
+++ Examples/eg3_12.py
@@ -1,1 +1,7 @@
 import random as r
+digit = input("Digit: ")
+if digit.isnumeric():
+    digit = int(digit)
+    max = int("9" * digit)
+    rand = r.randint(0, max)
+    print(f"%d-digit random OTP = %0{digit}d" %(digit, rand))
