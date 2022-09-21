c593720f1bc14ca87d6cd90a5dd990b4b7eba976
Sai Thant Min Nyo <saithantminnyo2@gmail.com>
2022-09-21 21:02:58 +0700

    changes: in 1_10

--- lab_quiz/quiz1/no10.py
+++ lab_quiz/quiz1/no10.py
@@ -1,14 +1,21 @@
 dna = input("DNA: ")
 base = input("base: ")
-if base.isalpha() and dna.isalpha():
-    count = 0
+if base.isalpha() and dna.isalpha() and base in ["A", "C", "G", "T"]:
+    boo = True
     for ch in dna:
-        print("c: %s" % ch)
-        if ch == base:
-            print("True if test")
-            count += 1
-    if count > 0:
-        print(f"There are {count} times that the base {base} occur in this DNA.")
+        if ch not in ["A", "C", "G", "T"]:
+            boo = False
+    if boo:
+        count = 0
+        for ch in dna:
+            print("c: %s" % ch)
+            if ch == base:
+                print("True if test")
+                count += 1
+        if count > 0:
+            print(f"There are {count} times that the base {base} occur in this DNA.")
+        else:
+            print("This is not DNA String.")
     else:
         print("This is not DNA String.")
 else:
