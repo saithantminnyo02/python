c0c9abfdd5a7cb563f0cb223a1b92f5e26ca2268
Sai Thant Min Nyo <saithantminnyo2@gmail.com>
2022-09-21 14:55:02 +0700

    add 2_5

--- lab_quiz/quiz2/no5.py
+++ lab_quiz/quiz2/no5.py
@@ -1,4 +1,11 @@
-a, b, c = "True True True".split(" ")
-print(a)
-print(b)
-print(c)
+a, b, c = input("a, b, c = ").split(" ")
+if (a == "True" or a == "False") and (b == "True" or b == "False") and (c == "True" or c == "False"):
+    if a == "True":
+        print("Grant")
+    else:
+        if b == "False" and c == "False":
+            print("Grant")
+        else:
+            print("Deny")
+else:
+    print("Invalid input")
