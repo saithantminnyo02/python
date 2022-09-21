d8ff5a42cd8079f2ea681a55c75a85591f1d24c4
Sai Thant Min Nyo <saithantminnyo2@gmail.com>
2022-09-21 15:30:45 +0700

    add 2_7

--- lab_quiz/quiz2/no7.py
+++ lab_quiz/quiz2/no7.py
@@ -3,7 +3,14 @@ a, b, c = input("Please enter an input: ").split("*")
 choice = input("Please enter your choice (1 or 2): ")
 if a.isnumeric() and b.isnumeric() and c.isnumeric() and choice.isnumeric() and (choice in ["1", "2"]):
     a, b, c = int(a), int(b), int(c)
-    if choice == "1":
-        ans = a + ()
+    if a > 0 and b > 0 and c > 0:
+        if choice == "1":
+            ans = a + math.sqrt((b**2) + (c**3))
+        else:
+            ab = int(str(a) + str(b))
+            ans = ab % c
+        print("The output is %.2f" % ans)
+    else:
+        print("Invalid Inputs")
 else:
     print("Invalid Inputs")
