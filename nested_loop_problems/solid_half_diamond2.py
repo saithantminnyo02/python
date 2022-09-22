81ab66b0b7be28211a49ddb4fe6155204db68966
Sai Thant Min Nyo <saithantminnyo2@gmail.com>
2022-09-22 14:03:02 +0700

    add: no1

---
+++ nested_loop_problems/solid_half_diamond2.py
@@ -0,0 +1,14 @@
+n = 4
+start = 1
+bol = False
+for i in range(1, n + n):
+    for j in range(1, i + 1):
+        if j <= n + n - i:
+            print(start, end = "")
+        if i >= 4:
+            bol = True
+    print()
+    if bol:
+        start -= 1
+    else:
+        start += 1
