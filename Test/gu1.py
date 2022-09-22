af6e2687007b60c950f6d36e1d43771442d0a31a
Sai Thant Min Nyo <saithantminnyo2@gmail.com>
2022-09-22 14:13:00 +0700

    fix: no1

--- Test/gu1.py
+++ Test/gu1.py
@@ -1,15 +1,12 @@
 remaining = input("Input a remaining battery percentage: ")
 per_min = input("Input a battery draining per minute: ")
 
-if remaining and per_min:
-    remaining, per_min = float(remaining), float(per_min)
-    if remaining > 0 and per_min > 0:
-        to_sec = per_min / 60
-        secs = remaining / to_sec
-        mins = secs / 60
-        secs = secs % 60
-        print("The remaing time is %d minutes and %d seconds." % (mins, secs))
-    else:
-        print("Please check your input values")
+remaining, per_min = float(remaining), float(per_min)
+if remaining > 0 and per_min > 0:
+    to_sec = per_min / 60
+    secs = remaining / to_sec
+    mins = secs / 60
+    secs = secs % 60
+    print("The remaining time is %d minutes and %d seconds." % (mins, secs))
 else:
     print("Please check your input values")
