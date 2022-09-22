664871a524a1c0ccf006125523604b08ec252a66
Sai Thant Min Nyo <saithantminnyo2@gmail.com>
2022-09-22 11:44:03 +0700

    add pyramid2.py

--- nested_loop_problems/pyramid2.py
+++ nested_loop_problems/pyramid2.py
@@ -3,5 +3,13 @@ col = n + n - 1
 counter = 1
 for i in range(1, n+1):
     for j in range(1, col + 1):
-        print(counter, end=" ")
+        if i > n - j and j - n < i:
+            print(counter, end=" ")
+            if j >= n:
+                counter -= 1
+            else:
+                counter += 1
+        else:
+            print(" ", end=" ")
     print()
+    counter = 1
