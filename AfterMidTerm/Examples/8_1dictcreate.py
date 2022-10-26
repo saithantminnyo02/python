e66a963d8452018798fff80e63b88b4ab0d9060a
Sai Thant Min Nyo <saithantminnyo2@gmail.com>
2022-10-26 22:00:00 +0700

    8.1 and 8.2

---
+++ AfterMidTerm/Examples/8_1dictcreate.py
@@ -0,0 +1,7 @@
+empty_dict = {}
+math_constants = {'pi': 3.14, 'e': 2.71}
+n_students = {1: 15, 2: 19, 3: 18}
+
+print(empty_dict)
+print(math_constants)
+print(n_students)
---
+++ AfterMidTerm/Examples/8_2dictaccess.py
@@ -0,0 +1,3 @@
+course = {'code': 'ITS100', 'semester': 2, 'acad year': 2021}
+
+print(course['code'])
